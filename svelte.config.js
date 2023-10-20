f05eec4457c7e0b075afb95b656c357d56ad3b6c
Matthew Schwencke <matt.schwencke@proton.me>
2023-10-20 15:06:05 +0900

    Added kit to paths

--- svelte.config.js
+++ svelte.config.js
@@ -21,6 +21,6 @@ const config = {
 	}
 };
 
-config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
+config.kit.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 
 export default config;
